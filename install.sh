2f2a463348e29f3a09687da05e87c38d6274a846
Alexander Preibisch <alexpreib@outlook.com>
2022-01-29 16:44:05 +0100

    Update install script output

--- install.sh
+++ install.sh
@@ -7,3 +7,20 @@ git clone https://github.com/TheAxelander/wtf.git ~/.local/wtf
 
 echo "Copy wtf script to /usr/local/bin"
 sudo cp ~/.local/wtf/wtf /usr/local/bin/wtf
+
+cat <<-'EOM'
+
+           _    __     _           _        _ _          _ 
+          | |  / _|   (_)         | |      | | |        | |
+ __      _| |_| |_     _ _ __  ___| |_ __ _| | | ___  __| |
+ \ \ /\ / / __|  _|   | | '_ \/ __| __/ _` | | |/ _ \/ _` |
+  \ V  V /| |_| |     | | | | \__ \ || (_| | | |  __/ (_| |
+   \_/\_/  \__|_|     |_|_| |_|___/\__\__,_|_|_|\___|\__,_|
+                                                           
+                                                           
+WTF has been installed. Help files are located in ~/.local/wtf/files
+You can run WTF with:
+
+wtf
+
+EOM
